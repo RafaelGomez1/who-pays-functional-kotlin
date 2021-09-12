aaf94b6f90572d601c21f88ac527ac6f7a5f4b5e
Rafa <rokitoki97@gmail.com>
2021-09-12 18:28:45 +0200

    ADD unit tests to github flow

---
+++ src/test/kotlin/rgomez/whopayskotlin/RandomTest.kt
@@ -0,0 +1,12 @@
+package rgomez.whopayskotlin
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class RandomTest {
+
+    @Test
+    fun contextLoads() {
+        Assertions.assertTrue(true)
+    }
+}
